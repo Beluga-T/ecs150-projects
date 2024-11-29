9c42acafc9b36bb81ce81d9a81464a1acab83a7a
beluga-T <tswlovexyb@outlook.com>
2024-11-29 11:07:07 +0000

    Refactor LocalFileSystem methods for consistent formatting and style

--- project4/gunrock_web/LocalFileSystem.cpp
+++ project4/gunrock_web/LocalFileSystem.cpp
@@ -8,60 +8,65 @@
 
 using namespace std;
 
-
-LocalFileSystem::LocalFileSystem(Disk *disk) {
+LocalFileSystem::LocalFileSystem(Disk *disk)
+{
   this->disk = disk;
 }
 
-void LocalFileSystem::readSuperBlock(super_t *super) {
-
+void LocalFileSystem::readSuperBlock(super_t *super)
+{
 }
 
-void LocalFileSystem::readInodeBitmap(super_t *super, unsigned char *inodeBitmap) {
-
+void LocalFileSystem::readInodeBitmap(super_t *super, unsigned char *inodeBitmap)
+{
 }
 
-void LocalFileSystem::writeInodeBitmap(super_t *super, unsigned char *inodeBitmap) {
-
+void LocalFileSystem::writeInodeBitmap(super_t *super, unsigned char *inodeBitmap)
+{
 }
 
-void LocalFileSystem::readDataBitmap(super_t *super, unsigned char *dataBitmap) {
-
+void LocalFileSystem::readDataBitmap(super_t *super, unsigned char *dataBitmap)
+{
 }
 
-void LocalFileSystem::writeDataBitmap(super_t *super, unsigned char *dataBitmap) {
-
+void LocalFileSystem::writeDataBitmap(super_t *super, unsigned char *dataBitmap)
+{
 }
 
-void LocalFileSystem::readInodeRegion(super_t *super, inode_t *inodes) {
-
+void LocalFileSystem::readInodeRegion(super_t *super, inode_t *inodes)
+{
 }
 
-void LocalFileSystem::writeInodeRegion(super_t *super, inode_t *inodes) {
-
+void LocalFileSystem::writeInodeRegion(super_t *super, inode_t *inodes)
+{
 }
 
-int LocalFileSystem::lookup(int parentInodeNumber, string name) {
+int LocalFileSystem::lookup(int parentInodeNumber, string name)
+{
   return 0;
 }
 
-int LocalFileSystem::stat(int inodeNumber, inode_t *inode) {
+int LocalFileSystem::stat(int inodeNumber, inode_t *inode)
+{
   return 0;
 }
 
-int LocalFileSystem::read(int inodeNumber, void *buffer, int size) {
+int LocalFileSystem::read(int inodeNumber, void *buffer, int size)
+{
   return 0;
 }
 
-int LocalFileSystem::create(int parentInodeNumber, int type, string name) {
+int LocalFileSystem::create(int parentInodeNumber, int type, string name)
+{
   return 0;
 }
 
-int LocalFileSystem::write(int inodeNumber, const void *buffer, int size) {
+int LocalFileSystem::write(int inodeNumber, const void *buffer, int size)
+{
   return 0;
 }
 
-int LocalFileSystem::unlink(int parentInodeNumber, string name) {
+int LocalFileSystem::unlink(int parentInodeNumber, string name)
+{
   return 0;
 }
-
